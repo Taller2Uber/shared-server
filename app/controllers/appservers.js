0dcff2e9853684a548f74e23d4b5a613ab9f92ab
gustavoadriangimenez <gustavoadriangimenez@gmail.com>
2017-09-03 16:46:43 -0300

    Agrego generacion de token y obtencion de fecha de creacion

--- app/controllers/appservers.js
+++ app/controllers/appservers.js
@@ -1,10 +1,12 @@
 var idGlobal;
+var randtoken = require('rand-token');
 
 exports.getAll = function( req, res ) {
     res.send( 'Listar todos los appservers' );
 };
 
 exports.create = function( name, res ){
-
-    res.send('name: '+ name + ' ,token: ');
+    var token = randtoken.generate(32);
+    var fecha = new Date().toLocaleString();
+    res.send('name: '+ name + ', token: ' + token + ", fecha de creacion: " + fecha);
 };
